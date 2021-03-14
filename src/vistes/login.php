68cc1cf5ac148960dead896333ff7cdc44b42409
sikuu <sikuu@Sikuu-PC.proxmox.com>
2021-03-14 11:19:00 +0100

    v1.1

--- src/vistes/login.php
+++ src/vistes/login.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link href="../public/estils.css" rel="stylesheet" type="text/css" media="all"/>
+    <link href="estils.css" rel="stylesheet" type="text/css" media="all"/>
     <title>Entra</title>
 </head>
 <body>
